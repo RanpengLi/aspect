e652df01de84236baec94a0c4dd14a7a58055931
Ranpeng Li <ranpeng.li.geol@gmail.com>
2024-10-25 11:55:59 +0200

    add ave entropy visulization

---
+++ source/postprocess/visualization/entropy_average.cc
@@ -0,0 +1,81 @@
+/*
+  Copyright (C) 2011 - 2022 by the authors of the ASPECT code.
+
+  This file is part of ASPECT.
+
+  ASPECT is free software; you can redistribute it and/or modify
+  it under the terms of the GNU General Public License as published by
+  the Free Software Foundation; either version 2, or (at your option)
+  any later version.
+
+  ASPECT is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU General Public License for more details.
+
+  You should have received a copy of the GNU General Public License
+  along with ASPECT; see the file LICENSE.  If not see
+  <http://www.gnu.org/licenses/>.
+*/
+
+
+#include <aspect/postprocess/visualization/entropy_average.h>
+#include <aspect/adiabatic_conditions/interface.h>
+
+
+namespace aspect
+{
+  namespace Postprocess
+  {
+    namespace VisualizationPostprocessors
+    {
+      template <int dim>
+      EntropyAverage<dim>::
+      EntropyAverage ()
+        :
+        DataPostprocessorScalar<dim> ("entropy_average",
+                                      update_values | update_quadrature_points),
+        Interface<dim>("K")
+      {}
+
+
+
+      template <int dim>
+      void
+      EntropyAverage<dim>::
+      evaluate_vector_field(const DataPostprocessorInputs::Vector<dim> &input_data,
+                            std::vector<Vector<double>> &computed_quantities) const
+      {
+        const unsigned int n_quadrature_points = input_data.solution_values.size();
+        Assert (computed_quantities.size() == n_quadrature_points,    ExcInternalError());
+        Assert (computed_quantities[0].size() == 1,                   ExcInternalError());
+        Assert (input_data.solution_values[0].size() == this->introspection().n_components,           ExcInternalError());
+
+        for (unsigned int q=0; q<n_quadrature_points; ++q)
+          {
+            const double entropy = input_data.solution_values[q][this->introspection().component_indices.entropy];
+
+            computed_quantities[q](0) = entropy - this->get_adiabatic_conditions().temperature(input_data.evaluation_points[q]);
+          }
+      }
+    }
+  }
+}
+
+
+// explicit instantiations
+namespace aspect
+{
+  namespace Postprocess
+  {
+    namespace VisualizationPostprocessors
+    {
+      ASPECT_REGISTER_VISUALIZATION_POSTPROCESSOR(EntropyAverage,
+                                                  "entropy average",
+                                                  "A visualization output object that generates output "
+                                                  "for the averaged entropy of multiple components."
+                                                  "\n\n"
+                                                  "Physical units: \\si{\\J/kg/K}.")
+    }
+  }
+}
