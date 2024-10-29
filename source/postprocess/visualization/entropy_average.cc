24149ca7fa9b15a13b238f54494116db249aa31c
Ranpeng Li <ranpeng.li.geol@gmail.com>
2024-10-29 15:43:38 +0100

    wip

--- source/postprocess/visualization/entropy_average.cc
+++ source/postprocess/visualization/entropy_average.cc
@@ -51,12 +51,29 @@ namespace aspect
         Assert (computed_quantities[0].size() == 1,                   ExcInternalError());
         Assert (input_data.solution_values[0].size() == this->introspection().n_components,           ExcInternalError());
 
+        std::vector<unsigned int> chemical_composition_idx;
+        chemical_composition_idx = this->introspection().chemical_composition_field_indices();
+//std::cout << "chemical_composition_idx " << chemical_composition_idx.size() <<std::endl;
+
+        std::vector<unsigned int> entropy_field_idx;
+        entropy_field_idx = this->introspection().get_indices_for_fields_of_type(CompositionalFieldDescription::chemical_composition);
+
+        Assert(chemical_composition_idx.size() == entropy_field_idx.size(),
+                    ExcMessage("The 'entropy model' material model assumes that there exists a background field in addition to the compositional fields, "
+                               "and therefore it requires one more lookup table than there are chemical compositional fields."));
+
+
         for (unsigned int q=0; q<n_quadrature_points; ++q)
           {
-            const double entropy = input_data.solution_values[q][this->introspection().component_indices.compositional_fields[sets[0][0]]]; //sets = vector that contain entropy-related data?
+            double entropy = 0;
+                                   
+            for (unsigned int i=0; i<entropy_field_idx.size(); ++i)
+                  entropy = entropy + input_data.solution_values[q][chemical_composition_idx[i]] * input_data.solution_values[q][entropy_field_idx[i]];
 
-            computed_quantities[q](0) = entropy - this->get_adiabatic_conditions().temperature(input_data.evaluation_points[q]);
-            // = mass_fractions i * entropy i
+            // const double entropy = input_data.solution_values[q][this->introspection().component_indices.compositional_fields[sets[0][0]]]; //sets = vector that contain entropy-related data?
+
+            computed_quantities[q](0) = entropy;    //entropy - this->get_adiabatic_conditions().temperature(input_data.evaluation_points[q]);
+                                        // = mass_fractions i * entropy i
           }
       }
     }
